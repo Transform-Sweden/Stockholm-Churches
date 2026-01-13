a417c1b4b7c690dbe92f554d55f00a210362e8a2
Silvano <wd7znyqb8j@privaterelay.appleid.com>
2026-01-13 13:02:07 +0100

    Update churches.js

--- churches.js
+++ churches.js
@@ -2,16 +2,16 @@
 const churches = [
   {
     id: "citykyrkan",
-    name: { sv: "Citykyrkan i Linköping", en: "Citykyrkan (City Church) Linköping" },
-    address: "S:t Larsgatan 19, 582 24 Linköping",
-    lat: 58.412719,
-    lng: 15.624481,
+    name: { sv: "Citykyrkan i Stockholm", en: "Citykyrkan (City Church) Stockholm" },
+    address: "Adolf Fredriks kyrkogata 10, 111 37 Stockholm",
+    lat: 59.33741816439673,
+    lng: 18.0593015,
     coords_verified: true,
     description: {
       sv: "En öppen, Jesus-centrerad församling mitt i stan (EFK). Varm gemenskap och evangeliskt fokus.",
       en: "A warm, Jesus-centered downtown church (EFK) with a strong evangelistic focus."
     },
-    website: "https://citykyrkan.nu/"
+    website: "https://cks.se/"
   },
   {
     id: "equmenia",
